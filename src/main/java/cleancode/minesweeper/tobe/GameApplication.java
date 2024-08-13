1b72a49cde258c4900ffcc2774fba35add4cee92
tngh4037 <tngh4037@gmail.com>
2024-08-14 03:36:37 +0900

    section3: SOLID - OCP(Open-Closed Principle)

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,10 +1,16 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.gamelevel.Advanced;
+import cleancode.minesweeper.tobe.gamelevel.Beginner;
+import cleancode.minesweeper.tobe.gamelevel.GameLevel;
+import cleancode.minesweeper.tobe.gamelevel.Middle;
+
 // 게임을 실행하는 책임
 public class GameApplication {
 
     public static void main(String[] args) {
-        Minesweeper minesweeper = new Minesweeper();
+        GameLevel gameLevel = new Advanced();
+        Minesweeper minesweeper = new Minesweeper(gameLevel);
         minesweeper.run();
     }
 }
--- src/main/java/cleancode/minesweeper/tobe/io/ConsoleOutputHandler.java
+++ src/main/java/cleancode/minesweeper/tobe/io/ConsoleOutputHandler.java
@@ -3,6 +3,9 @@ package cleancode.minesweeper.tobe.io;
 import cleancode.minesweeper.tobe.GameBoard;
 import cleancode.minesweeper.tobe.GameException;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 // 출력을 담당하는 책임
 public class ConsoleOutputHandler {
 
@@ -13,15 +16,26 @@ public class ConsoleOutputHandler {
     }
 
     public void showBoard(GameBoard board) {
-        System.out.println("   a b c d e f g h i j");
+        String joiningAlphabets = generateColAlphabets(board);
+        System.out.println("    " + joiningAlphabets);
+
         for (int row = 0; row < board.getRowSize(); row++) {
-            System.out.printf("%d  ", row + 1);
+            System.out.printf("%2d  ", row + 1);
             for (int col = 0; col < board.getColSize(); col++) {
                 System.out.print(board.getSign(row, col) + " ");
             }
             System.out.println();
         }
         System.out.println();
+    }
+
+    private String generateColAlphabets(GameBoard board) {
+        List<String> alphabets = IntStream.range(0, board.getColSize())
+                .mapToObj(index -> (char) ('a' + index))
+                .map(c -> c.toString())
+                .toList();
+
+        return String.join(" ", alphabets);
     }
 
     public void printGameWinningComment() {
