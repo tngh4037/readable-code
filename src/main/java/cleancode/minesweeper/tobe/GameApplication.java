eb51827a566d10a7776ebcba82f3c93ca81dbbe7
tngh4037 <tngh4037@gmail.com>
2024-08-21 01:20:29 +0900

    section3: SOLID - DIP(Dependency Inversion Principle)

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -4,15 +4,33 @@ import cleancode.minesweeper.tobe.gamelevel.Advanced;
 import cleancode.minesweeper.tobe.gamelevel.Beginner;
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
 import cleancode.minesweeper.tobe.gamelevel.Middle;
+import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
+import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
+import cleancode.minesweeper.tobe.io.InputHandler;
+import cleancode.minesweeper.tobe.io.OutputHandler;
 
 // 게임을 실행하는 책임
 public class GameApplication {
 
     public static void main(String[] args) {
         GameLevel gameLevel = new Beginner();
+        InputHandler inputHandler = new ConsoleInputHandler();
+        OutputHandler outputHandler = new ConsoleOutputHandler();
 
-        Minesweeper minesweeper = new Minesweeper(gameLevel);
+        Minesweeper minesweeper = new Minesweeper(gameLevel, inputHandler, outputHandler);
         minesweeper.initialize();
         minesweeper.run();
     }
+
+    /**
+     * DIP (Dependency Inversion Principle)
+     * - 고수준 모듈과 저수준 모듈이 서로 직접적으로 의존하지 않고, 추상화에 의존해야 한다. ( 인터페이스, 추상클래스 등 )
+     *
+     * DI (Dependency Injection)
+     * - 필요한 의존성을 내가 직접 생성하는 것이 아니라, 외부에서 주입받겠다는 의미.
+     *
+     * IoC (Inversion of Control)
+     * - 프로그램의 흐름을 개발자가 아닌, 프레임워크가 담당하도록 하는 것.
+     */
+
 }
--- src/main/java/cleancode/minesweeper/tobe/io/ConsoleInputHandler.java
+++ src/main/java/cleancode/minesweeper/tobe/io/ConsoleInputHandler.java
@@ -2,11 +2,11 @@ package cleancode.minesweeper.tobe.io;
 
 import java.util.Scanner;
 
-// 입력을 담당하는 책임
-public class ConsoleInputHandler {
+public class ConsoleInputHandler implements InputHandler {
 
     public static final Scanner SCANNER = new Scanner(System.in);
 
+    @Override
     public String getUserInput() {
         return SCANNER.nextLine();
     }
