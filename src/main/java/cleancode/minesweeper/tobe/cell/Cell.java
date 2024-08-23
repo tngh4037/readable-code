c66377164e195c635c4cf0e142a1124c66ba4b6a
tngh4037 <tngh4037@gmail.com>
2024-08-24 07:46:58 +0900

    section5: 객제 지향 적용하기 - 상속과 조합

--- src/main/java/cleancode/minesweeper/tobe/cell/Cell.java
+++ src/main/java/cleancode/minesweeper/tobe/cell/Cell.java
@@ -1,36 +1,22 @@
 package cleancode.minesweeper.tobe.cell;
 
-public abstract class Cell {
+public interface Cell {
 
-    protected static final String FLAG_SIGN = "⚑";
-    protected static final String UNCHECKED_SIGN = "□";
+    String FLAG_SIGN = "⚑";
+    String UNCHECKED_SIGN = "□";
 
-    protected boolean isFlagged;
-    protected boolean isOpened;
+    boolean isLandMine();
 
-    public abstract boolean isLandMine();
+    boolean hasLandMineCount();
 
-    public abstract boolean hasLandMineCount();
+    String getSign();
 
-    public abstract String getSign();
+    void flag();
 
-    public void flag() {
-        this.isFlagged = true;
-    }
+    void open();
 
-    public void open() {
-        isOpened = true;
-    }
+    boolean isChecked();
 
-    public boolean isChecked() {
-        return isFlagged || isOpened; // 닫혀있는지 깃발이 꽂혀있거나 or 진짜 열었거나
-    }
+    boolean isOpened();
 
-    public boolean isOpened() {
-        return isOpened;
-    }
 }
-
-// 참고) 정적 팩토리 메소드
-// new Cell(..)과 같이 객체를 만들 수도 있지만, 정적 팩토리 메소드로 이름을 주면서 만드는 것도 좋다.
-// ( 비슷한 형태의 정적 팩토리 메서드 여러개가 생기면, 그에 따른 적절한 이름들을 각각 줄수도 있고, 검증 같은 것도 그에 맞게 해줄 수 있기 때문이다. )
