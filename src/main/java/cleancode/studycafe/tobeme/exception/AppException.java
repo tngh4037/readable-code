6980bc0fb47bc7c87a220b9b4f473580ee1a27f9
tngh4037 <tngh4037@gmail.com>
2024-09-12 22:51:57 +0900

    section7: 리팩토링 연습 - 개별 연습을 위한 패키지 복사

---
+++ src/main/java/cleancode/studycafe/tobeme/exception/AppException.java
@@ -0,0 +1,9 @@
+package cleancode.studycafe.tobeme.exception;
+
+public class AppException extends RuntimeException {
+
+    public AppException(String message) {
+        super(message);
+    }
+
+}
